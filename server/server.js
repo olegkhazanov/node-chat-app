eecc6611a2e2c2084c9eacd85bf6ba3ee4889014
Oleg Khazanov <okhazanov@gmail.com>
2018-12-17 18:03:28 -0800

    socket io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+  console.log('Net user connected');
+
+  socket.on('disconnect', () =>{
+    console.log('Disconnected');
+  });
+});
+
+
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
