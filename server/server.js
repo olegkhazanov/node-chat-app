e0736397708cb88efc65904684cb31c1f5f786f3
Oleg Khazanov <okhazanov@gmail.com>
2018-12-18 11:33:27 -0800

    Emit events

--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
   console.log('Net user connected');
 
-  socket.emit('newMessage', {
-      from: 'server',
-      text: 'Hey. What is going on.',
-      createAt: 123
-    });
-
     socket.on('createMessage', (message) => {
       console.log('createMessage', message);
+
+      io.emit('newMessage',{
+        from: message.from,
+        text: message.text,
+        createAt: new Date().getTime()
+      });
     });
 
 
